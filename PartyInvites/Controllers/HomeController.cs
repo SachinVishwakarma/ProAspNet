680d1c8ee3d1b4df2fbd704ada2616f3bc7e9f9d
Sachin Vishwakarma <vsachin@vegam.co>
2021-06-26 22:57:49 +0530

    Party Invites Commit first

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,27 +11,29 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // TODO: Handle incoming data here
+            Repository.AddResponse(guestResponse);
+
+            return View("Thanks", guestResponse);
+        }
+
+        public ViewResult ListResponse()
+        {
+            return View(Repository.Responses.Where(x => x.WillAttend == true));
         }
     }
 }
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string EmailID { get; set; }
+        public string Mobile { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
